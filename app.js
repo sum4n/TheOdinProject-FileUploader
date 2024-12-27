875c82036de1d3e42d844f3dc565cfade3a3a8f3
Suman Das <sum4ndas@gmail.com>
2024-12-27 15:04:02 +0530

    Refactor app.js, move passportjs code in its seperate module

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const prisma = new PrismaClient();
 
 const express = require("express");
 const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 
 const bcrypt = require("bcryptjs");
+
+const passport = require("./config/passport");
 
 const app = express();
 // Set views path and view engine
@@ -18,55 +18,6 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
-
-// Authentication: Setting up LocalStrategy.
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await prisma.user.findUnique({
-        where: {
-          username: username,
-        },
-      });
-      // console.log(user);
-
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      // if (user.password !== password) {
-      //   return done(null, flase, { message: "Incorrect password" });
-      // }
-      // Compare hashed password.
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: "Incorect password" });
-      }
-
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
-
-// Authentication: Sessions and serialization.
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: id,
-      },
-    });
-    // console.log(user);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
 
 // Custom middleware that simplifies access to the currentUser in views.
 // This enables access to the currentUser variable in all views without manually passing it in controllers.
---
+++ config/passport.js
@@ -0,0 +1,55 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const bcrypt = require("bcryptjs");
+
+// Authentication: Setting up LocalStrategy.
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          username: username,
+        },
+      });
+      // console.log(user);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      // Compare hashed password.
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+// Authentication: Sessions and serialization.
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    // console.log(user);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
