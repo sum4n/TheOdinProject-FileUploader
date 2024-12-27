780647b4523957a8a9b60751eebe139f02cedf26
Suman Das <sum4ndas@gmail.com>
2024-12-27 16:47:35 +0530

    Add session migration and refactor session.js to use prisma-session-store

--- config/session.js
+++ config/session.js
@@ -1,7 +1,17 @@
 const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 
 module.exports = session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  store: new PrismaSessionStore(new PrismaClient(), {
+    checkPeriod: 2 * 60 * 1000, //ms
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined,
+  }),
+  cookie: {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+  },
 });
