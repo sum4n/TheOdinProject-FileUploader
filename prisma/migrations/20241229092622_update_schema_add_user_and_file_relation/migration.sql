82811dcd742f4139c6fab18c3f58fbedf8b08e29
Suman Das <sum4ndas@gmail.com>
2024-12-29 14:56:55 +0530

    Update Schema: Add User and File relation

---
+++ prisma/migrations/20241229092622_update_schema_add_user_and_file_relation/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "File" ADD COLUMN     "ownerId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
