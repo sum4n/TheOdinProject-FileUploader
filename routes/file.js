5ce62d2246d8f1d45e4280e40c86b38271b7858e
Suman Das <sum4ndas@gmail.com>
2025-01-02 19:28:17 +0530

    Create file.js router and move file routes into it.

---
+++ routes/file.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// Multer
+const upload = require("../config/multer");
+
+// Get upload file form
+router.get("/upload-file", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("upload-file-form");
+  } else {
+    res.render("sign-up-form");
+  }
+});
+
+// Post upload file form.
+router.post(
+  "/upload-file/:directoryId",
+  upload.single("uploaded-file"),
+  async (req, res, next) => {
+    // console.table(req.file);
+    // console.log(req.params);
+
+    await prisma.file.create({
+      data: {
+        name: req.file.originalname,
+        size: req.file.size.toString(),
+        type: req.file.mimetype,
+        path: "./uploads/" + req.file.originalname,
+        directoryId: parseInt(req.params.directoryId),
+        ownerId: req.user.id,
+      },
+    });
+    res.redirect("/");
+  }
+);
+
+module.exports = router;
