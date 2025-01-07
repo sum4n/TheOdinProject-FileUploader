a4841664af57ddbe5a30fec28274467f0e22093a
Suman Das <sum4ndas@gmail.com>
2025-01-07 16:24:56 +0530

    Import and use 'express-async-handler' in all fileController methods

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,10 +1,13 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
+
 // Multer
 // const upload = require("../config/multer");
 
-module.exports.createFile = async (req, res, next) => {
+module.exports.createFile = asyncHandler(async (req, res, next) => {
   // console.table(req.file);
   // console.log(req.params);
 
@@ -20,9 +23,9 @@ module.exports.createFile = async (req, res, next) => {
   });
   // res.redirect(`/directory/${parseInt(req.params.directoryId)}`);
   res.redirect(req.get("referer"));
-};
+});
 
-module.exports.getFileInfo = async (req, res) => {
+module.exports.getFileInfo = asyncHandler(async (req, res) => {
   const file = await prisma.file.findUnique({
     where: {
       id: parseInt(req.params.fileId),
@@ -33,9 +36,9 @@ module.exports.getFileInfo = async (req, res) => {
   res.render("file-info", {
     file,
   });
-};
+});
 
-module.exports.deleteFile = async (req, res) => {
+module.exports.deleteFile = asyncHandler(async (req, res) => {
   const file = await prisma.file.delete({
     where: {
       id: parseInt(req.params.fileId),
@@ -45,4 +48,4 @@ module.exports.deleteFile = async (req, res) => {
 
   // res.send(JSON.stringify(file.directoryId));
   res.redirect(`/directory/${file.directoryId}`);
-};
+});
