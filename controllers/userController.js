b2a5ab96862dabde37e4e1d53a22a81d961e488d
Suman Das <sum4ndas@gmail.com>
2025-01-09 20:46:40 +0530

    Write sign up form validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,64 @@ const prisma = new PrismaClient();
 const bcrypt = require("bcryptjs");
 const passport = require("../config/passport");
 
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contains letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUserSignUp = [
+  body("username")
+    .trim()
+    .isAlpha()
+    .withMessage(`Username ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Username ${lengthErr}`)
+    .escape(),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required.")
+    .isEmail()
+    .withMessage("Invalid email format.")
+    .normalizeEmail(),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 4, max: 10 })
+    .withMessage("Password must be between 4 and 10 characters.")
+    .not()
+    .matches(/\s/)
+    .withMessage("Password must not contain spaces."),
+];
+
 module.exports.getSignUp = (req, res) => {
-  res.render("sign-up-form");
+  const data = {}; // for view
+  res.render("sign-up-form", {
+    data,
+  });
 };
 
-module.exports.postSignUp = async (req, res, next) => {
-  try {
+module.exports.postSignUp = [
+  validateUserSignUp,
+  asyncHandler(async (req, res) => {
+    // Extract validation errors from request
+    const errors = validationResult(req);
+
+    const data = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("sign-up-form", {
+        data,
+        errors: errors.array(),
+      });
+    }
+
     // Hash the password
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
@@ -24,10 +76,8 @@ module.exports.postSignUp = async (req, res, next) => {
       },
     });
     res.redirect("/");
-  } catch (err) {
-    return next(err);
-  }
-};
+  }),
+];
 
 module.exports.postLogIn = (req, res, next) => {
   passport.authenticate("local", {
