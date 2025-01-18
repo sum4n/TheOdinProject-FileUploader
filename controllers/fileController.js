30940018a8a91aeac03a2b21db87d10f40a0d0b0
Suman Das <sum4ndas@gmail.com>
2025-01-19 00:08:11 +0530

    Add downloadFile controller with error handling

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,11 +1,10 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const path = require("node:path");
+const fs = require("fs");
 
 const asyncHandler = require("express-async-handler");
 const CustomNotFoundError = require("../errors/CustomNotFoundError");
-
-// Multer
-// const upload = require("../config/multer");
 
 module.exports.createFile = asyncHandler(async (req, res, next) => {
   // console.table(req.file);
@@ -66,4 +65,33 @@ module.exports.deleteFile = asyncHandler(async (req, res) => {
 
   // res.send(JSON.stringify(file.directoryId));
   res.redirect(`/directory/${file.directoryId}`);
+});
+
+module.exports.downloadFile = asyncHandler(async (req, res) => {
+  const file = await prisma.file.findUnique({
+    where: {
+      id: parseInt(req.params.fileId),
+      ownerId: req.user.id,
+    },
+  });
+
+  if (!file) {
+    throw new CustomNotFoundError("File does not exist");
+  }
+
+  const fileName = file.name;
+  const directoryPath = path.join(__dirname, "../uploads");
+  const filePath = `${directoryPath}/${fileName}`;
+
+  // handling errors
+  if (fs.existsSync(filePath)) {
+    res.download(filePath, fileName, (err) => {
+      if (err) {
+        console.error(`Error downloading the file: ${err.message}`);
+        res.status(500).send("Could not download the file.");
+      }
+    });
+  } else {
+    res.status(404).send("File not found");
+  }
 });
