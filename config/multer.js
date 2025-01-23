d560037a34c9cecab4574d99f3291f15b5440d84
Suman Das <sum4ndas@gmail.com>
2025-01-23 22:37:44 +0530

    Refactor multer.js - more file upload logic to fileController

--- config/multer.js
+++ config/multer.js
@@ -1,27 +1,7 @@
 const multer = require("multer");
-const fs = require("node:fs");
-const path = require("node:path");
 
-const storage = multer.diskStorage({
-  destination: async function (req, file, cb) {
-    // Dynamic directory for uploads.
-    const uploadPath = path.join(
-      __dirname,
-      "../uploads",
-      `${req.user.username}/${req.params.directoryId}`
-    );
-    // console.log(uploadPath);
-
-    // Create the directory if it does not exist
-    fs.mkdirSync(uploadPath, { recursive: true });
-
-    cb(null, uploadPath); // Destination folder
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname); // Keep original name.
-  },
-});
-
+// Multer config
+const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
@@ -29,25 +9,7 @@ const upload = multer({
   },
 });
 
-// Middleware for handling Multer errors
-const uploadSingleFile = (req, res, next) => {
-  const uploadSingle = upload.single("uploaded-file"); // 'uploaded-file' is the field name
-
-  uploadSingle(req, res, (err) => {
-    if (err) {
-      if (err.code === "LIMIT_FILE_SIZE") {
-        // console.error(err);
-        return res.status(400).render("errors", {
-          err: { message: "File size exceeds the limit of 100kb." },
-        });
-      }
-      // Handle other Multer-specific errors
-      return res.status(400).render("errors", {
-        err: { message: err.message },
-      });
-    }
-    next(); // Proceed to the controller if no errors
-  });
-};
+// Middleware for handling file uploads
+const uploadSingleFile = upload.single("uploaded-file");
 
 module.exports = { uploadSingleFile };
