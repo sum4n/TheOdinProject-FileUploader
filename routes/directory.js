243f6cee2bdc539c268138d1b8eef4f9020c8e76
Suman Das <sum4ndas@gmail.com>
2025-06-10 18:52:27 +0530

    Import and use authMiddleware in directory routers

--- routes/directory.js
+++ routes/directory.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+const { isAuth } = require("../middlewares/authMiddleware");
 const directoryController = require("../controllers/directoryController");
 
 // Create directory
-router.post("/create/:parentDirectoryId", directoryController.createDirectory);
+router.post(
+  "/create/:parentDirectoryId",
+  isAuth,
+  directoryController.createDirectory
+);
 
 // Get directory
-router.get("/:directoryId", directoryController.getDirectory);
+router.get("/:directoryId", isAuth, directoryController.getDirectory);
 
 // Delete directory
-router.post("/delete/:directoryId", directoryController.deleteDirectory);
+router.post(
+  "/delete/:directoryId",
+  isAuth,
+  directoryController.deleteDirectory
+);
 
 module.exports = router;
